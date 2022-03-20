7e1e8034fbaa2dc245c63ada28dda7a6ccdf27b6
Xeba111 <sebasmena72@yahoo.com>
2022-03-20 09:30:31 -0500

    Probando push

--- app/src/main/java/com/example/proyecto4_mena/MainActivity.java
+++ app/src/main/java/com/example/proyecto4_mena/MainActivity.java
@@ -3,11 +3,15 @@ package com.example.proyecto4_mena;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        int n = 10;
     }
 }
