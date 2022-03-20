9e7956327e289a6f2053766022a071e4bb88f186
Xeba111 <sebasmena72@yahoo.com>
2022-03-20 10:02:18 -0500

    Boton y actualizacion

--- app/src/main/java/com/example/proyecto4_mena/MainActivity.java
+++ app/src/main/java/com/example/proyecto4_mena/MainActivity.java
@@ -2,9 +2,15 @@ package com.example.proyecto4_mena;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
 {
+    EditText inputTextX;
+    EditText inputTextY;
+    TextView mostrarXY;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -12,6 +18,24 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        int n = 10;
+        inputTextX = (EditText) findViewById(R.id.InputTextX);
+        inputTextY = (EditText) findViewById(R.id.InputTextY);
+        mostrarXY = (TextView) findViewById(R.id.imprimirCoordenadas);
+    }
+
+    public void guardarDatos(View view)
+    {
+        String valorX = inputTextX.getText().toString();
+        String valorY = inputTextY.getText().toString();
+        int x = 0;
+        int y = 0;
+
+        if(valorX != "" && valorY != "")
+        {
+            x = Integer.parseInt(valorX);
+            y = Integer.parseInt(valorY);
+        }
+
+        mostrarXY.setText("X: " + x + " ,Y: " + y);
     }
 }
